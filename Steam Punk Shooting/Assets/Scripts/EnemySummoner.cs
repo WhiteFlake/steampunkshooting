35f198b4217e9616ea33bdaab9b33943d0f817a2
Agustinho Pereira Neto <agustinho_iii@hotmail.com>
2019-02-28 03:19:16 -0300

    Summoner Done!

---
+++ Steam Punk Shooting/Assets/Scripts/EnemySummoner.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySummoner : Enemy
+{
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    private Vector2 targetPosition;
+    private Animator anim;
+
+    public float timeBetweenSummons;
+    private float summonTime;
+    public Enemy enemyToSummon;
+
+    public float attackSpeed;
+    private bool facinRight;
+    private float distance;
+    public float stopDistance;
+    private float attackTime;
+
+    public override void Start()
+    {
+        base.Start();
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        targetPosition = new Vector2(randomX, randomY);
+        anim = GetComponent<Animator>();
+        facinRight = true;
+}
+
+    private void Update()
+    {
+        if (player != null)
+        {
+            if (Vector2.Distance(transform.position, targetPosition) > 0.1f)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+                distance = targetPosition.x - transform.position.x;
+                Flip(distance);
+                anim.SetBool("isRunning", true);
+            }
+            else
+            {
+                anim.SetBool("isRunning", false);
+
+                if (Time.time >= summonTime)
+                {
+                    summonTime = Time.time + timeBetweenSummons;
+                    anim.SetTrigger("summon");
+                }
+            }
+
+            if (Vector2.Distance(transform.position, player.position) < stopDistance)
+            {
+                if (Time.time >= attackTime)
+                {
+                    StartCoroutine(Attack());
+                    attackTime = Time.time + timeBetweenAttacks;
+                }
+            }
+        }
+    }
+
+    public void Summon()
+    {
+        if(player != null)
+        {
+            Instantiate(enemyToSummon, transform.position, transform.rotation);
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        player.GetComponent<PlayerBehaviour>().TakeDamage(damage);
+
+        Vector2 originalPosition = transform.position;
+        Vector2 targetPosition = player.position;
+        float percent = 0;
+
+        while (percent <= 1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+            float formula = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector2.Lerp(originalPosition, targetPosition, formula);
+            yield return null;
+        }
+    }
+
+    public void Flip(float distance)
+    {
+        if (distance >= 0 && !facinRight || distance < 0 && facinRight)
+        {
+            facinRight = !facinRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+}
